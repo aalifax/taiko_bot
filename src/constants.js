2384860ccc2b9aa515fddddffb443bd9d404d20a
aalifax <112978313+aalifax@users.noreply.github.com>
2025-04-06 22:05:21 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const config = require('../config/config.json');
 
 module.exports = {
-    WETH_ADDRESS: process.env.WETH_CONTRACT_ADDRESS || "0xA51894664A773981C6C112C43ce576f315d5b1B6",
+    WETH_ADDRESS: process.env.WETH_CONTRACT_ADDRESS || "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
     VOTE_ADDRESS: process.env.VOTE_CONTRACT_ADDRESS || "0x4D1E2145082d0AB0fDa4a973dC4887C7295e21aB",
-    RPC_URL: process.env.RPC_URL || "https://rpc.taiko.tools/",
+    RPC_URL: process.env.RPC_URL || "https://eth.llamarpc.com/",
     REQUIRED_CONFIRMATIONS: config.confirmation.required,
     MAX_RETRIES: config.confirmation.maxRetries,
     RETRY_DELAY: config.confirmation.retryDelay,
